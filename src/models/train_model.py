741027b02aba7754a48279e381a41e05aaf19718
Noel Soares <noelsoares408@gmail.com>
2024-06-18 20:21:39 +0530

    train_model file created

---
+++ src/models/train_model.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import yaml
+import pathlib
+import joblib
+import sys
+from sklearn.ensemble import RandomForestClassifier
+
+def train_model(train_features, target, n_estimators, max_depth, seed):
+    #train the machine learning model
+    model=RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, random_state=seed)
+    model.fit(train_features, target)
+    return model
+
+def save_model(model, output_path):
+    #save the trained model to the specified output
+    joblib.dump(model,output_path +'/model.joblib')
+
+def main():
+    curr_dir = pathlib.Path(__file__)
+    home_dir = curr_dir.parent.parent.parent
+    params_file = home_dir.as_posix()+'/params.yaml'
+    params = yaml.safe_load(open(params_file))['train_model']
+    
+    input_path = sys.argv[1]
+    data_path = home_dir.as_posix()+input_path
+    output_path = home_dir.as_posix()+'/models'
+    pathlib.Path(output_path).mkdir(parents= True, exist_ok= True)
+    
+    TARGET='Class'
+    train_features = pd.read_csv(data_path+'/train.csv')
+    X = train_features.drop(TARGET,axis=1)
+    y = train_features[TARGET]
+    
+    trained_model = train_model(X,y,params['n_estimators'], params['max_depth'], params['seed'])
+    save_model(trained_model, output_path)
+
+
+if __name__=='__main__':
+    main()
